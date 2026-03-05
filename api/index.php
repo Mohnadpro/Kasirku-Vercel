185a4555fb3b0cd8a038e2a6fb90af3ed8ac9ab3
mohnadpro <mohnad775484998@gmail.com>
2026-03-05 15:58:29 +0300

    Auto-create sqlite file

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+// إنشاء قاعدة بيانات فارغة في المجلد المؤقت إذا لم تكن موجودة
+if (!file_exists('/tmp/database.sqlite')) {
+    touch('/tmp/database.sqlite');
+}
+
 require __DIR__ . '/../public/index.php';
